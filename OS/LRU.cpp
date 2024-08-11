a96d0fa270683e7834912a85caa177824dea628a
Firojpaudel <firojpaudel@gmail.com>
2024-08-11 17:05:32 +0545

    codes updated ..

--- OS/LRU.cpp
+++ OS/LRU.cpp
@@ -4,29 +4,36 @@
 #define MAX_FRAMES 100
 #define COLUMN_WIDTH 20
 
-void printFrames(int frames[], int frameSize) {
-    for (int i = 0; i < frameSize; i++) {
+void printFrames(int frames[], int frameSize)
+{
+    for (int i = 0; i < frameSize; i++)
+    {
         if (frames[i] != -1)
-            printf("%-*d", COLUMN_WIDTH / frameSize, frames[i]);  // Adjusting column width for better alignment
+            printf("%-*d", COLUMN_WIDTH / frameSize, frames[i]); // Adjusting column width for better alignment
         else
-            printf("%-*c", COLUMN_WIDTH / frameSize, '_');  // Adjusting column width for better alignment
+            printf("%-*c", COLUMN_WIDTH / frameSize, '_'); // Adjusting column width for better alignment
     }
 }
 
-int isPageInFrames(int frames[], int frameSize, int page) {
-    for (int i = 0; i < frameSize; i++) {
+int isPageInFrames(int frames[], int frameSize, int page)
+{
+    for (int i = 0; i < frameSize; i++)
+    {
         if (frames[i] == page)
             return 1;
     }
     return 0;
 }
 
-int findLRUVictim(int frameTimes[], int frameSize) {
+int findLRUVictim(int frameTimes[], int frameSize)
+{
     int minTime = frameTimes[0];
     int victim = 0;
 
-    for (int i = 1; i < frameSize; i++) {
-        if (frameTimes[i] < minTime) {
+    for (int i = 1; i < frameSize; i++)
+    {
+        if (frameTimes[i] < minTime)
+        {
             minTime = frameTimes[i];
             victim = i;
         }
@@ -35,14 +42,16 @@ int findLRUVictim(int frameTimes[], int frameSize) {
     return victim;
 }
 
-void lruPageReplacement(int pageReferences[], int numReferences, int frameSize) {
+void lruPageReplacement(int pageReferences[], int numReferences, int frameSize)
+{
     int frames[MAX_FRAMES];
     int frameTimes[MAX_FRAMES];
     int pageFaults = 0;
     int time = 0;
 
     // Initialize frames and times
-    for (int i = 0; i < frameSize; i++) {
+    for (int i = 0; i < frameSize; i++)
+    {
         frames[i] = -1;
         frameTimes[i] = 0;
     }
@@ -53,20 +62,40 @@ void lruPageReplacement(int pageReferences[], int numReferences, int frameSize) 
     printf("| Page Reference     | Frame Contents     | Page Fault |\n");
     printf("+--------------------+--------------------+------------+\n");
 
-    for (int i = 0; i < numReferences; i++) {
+    for (int i = 0; i < numReferences; i++)
+    {
         int page = pageReferences[i];
         int pageFault = !isPageInFrames(frames, frameSize, page);
 
         // Update frames if page fault occurs
-        if (pageFault) {
-            int victim = findLRUVictim(frameTimes, frameSize);
+        if (pageFault)
+        {
+            int victim = -1;
+            for (int j = 0; j < frameSize; j++)
+            {
+                if (frames[j] == -1)
+                {
+                    victim = j; // Empty frame found
+                    break;
+                }
+            }
+
+            if (victim == -1) // No empty frame, find LRU page to replace
+            {
+                victim = findLRUVictim(frameTimes, frameSize);
+            }
+
             frames[victim] = page;
             frameTimes[victim] = time;
             pageFaults++;
-        } else {
+        }
+        else
+        {
             // Update the time for the existing page
-            for (int j = 0; j < frameSize; j++) {
-                if (frames[j] == page) {
+            for (int j = 0; j < frameSize; j++)
+            {
+                if (frames[j] == page)
+                {
                     frameTimes[j] = time;
                     break;
                 }
@@ -87,7 +116,8 @@ void lruPageReplacement(int pageReferences[], int numReferences, int frameSize) 
     printf("\nTotal Page Faults: %d\n", pageFaults);
 }
 
-int main() {
+int main()
+{
     int numReferences, frameSize;
 
     printf("+------------------------------------+\n");
@@ -98,20 +128,23 @@ int main() {
     scanf("%d", &numReferences);
 
     int *pageReferences = (int *)malloc(numReferences * sizeof(int));
-    if (pageReferences == NULL) {
+    if (pageReferences == NULL)
+    {
         printf("Memory allocation failed.\n");
         return 1;
     }
 
     printf("Enter the page references: ");
-    for (int i = 0; i < numReferences; i++) {
+    for (int i = 0; i < numReferences; i++)
+    {
         scanf("%d", &pageReferences[i]);
     }
 
     printf("Enter the page frame size: ");
     scanf("%d", &frameSize);
 
-    if (frameSize <= 0 || frameSize > MAX_FRAMES) {
+    if (frameSize <= 0 || frameSize > MAX_FRAMES)
+    {
         printf("Invalid page frame size.\n");
         free(pageReferences);
         return 1;
