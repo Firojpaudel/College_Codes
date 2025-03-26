d29fe9d15eacec494da3358ca2d1b04e079f521f
FirozSama <98528410+Firojpaudel@users.noreply.github.com>
2025-03-26 17:22:00 +0545

    JS Folder updated as well.. Not full fledged tho

---
+++ Web_Tech/Classworks_folder/JS/intro.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Howdy there!");
+
+    var a = 20;
+    var b = 30.5;
+    var c = "ram";
+    var sum = a + b;
+    console.log("The sum is: " + sum);
+    
+    var x = prompt("Enter Name: ");
+    var y = prompt("Enter Surname: ");
+    console.log("Welcome! " + x + " " + y);
+});
